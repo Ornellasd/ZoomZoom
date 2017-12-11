fa2419f09f6a8c9e503408705c1c288084f279d9
David Ornellas <ornellasd@gmail.com>
2017-12-11 13:12:35 -0800

    added side boundary logic

--- ZoomZoom.py
+++ ZoomZoom.py
@@ -9,6 +9,7 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
+car_width = 78
  
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Zoom Zoom Zoom')
@@ -20,35 +21,42 @@ tankImg = pygame.image.load('tank.png')
 def car(x,y):
     gameDisplay.blit(tankImg,(x,y))
     
-x = (display_width * 0.45)
-y = (display_height * 0.65)
-x_change = 0
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.65)
 
-crashed = False
+    x_change = 0
 
-while not crashed:
+    gameExit = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+    while not gameExit:
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
 
-    x+= x_change                                                                                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
-    gameDisplay.fill(white)
-    car(x,y)
-    pygame.display.update()
-    clock.tick(60)
+        x+= x_change                                                                                    
 
+        gameDisplay.fill(white)
+        car(x,y)
+
+        if x > display_width - car_width or x < 0:
+            gameExit = True
+        
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
    
